255a3eecf9b44640a6bab80cc13dcf3ca08b0515
Łukasz Janiak <lucasekv2@gmail.com>
2024-06-17 05:46:08 +0200

    Remove game-id const

--- app/frontend/src/js/config.js
+++ app/frontend/src/js/config.js
@@ -1,3 +1,2 @@
 // config.js
 const apiGatewayUrl = "https://phr1rr1dec.execute-api.us-east-1.amazonaws.com/prod"; // Replace with your actual API Gateway URL
-const gameId = "your-game-id"; // Replace with the actual gameId
