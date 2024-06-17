eda783ce7c1857410476a1e5b22090d2b44831a1
Łukasz Janiak <lucasekv2@gmail.com>
2024-06-17 02:15:35 +0200

    Add DynamoDB integration

--- app/backend/src/main/java/com/arbuz/tictactoe/controller/GameController.java
+++ app/backend/src/main/java/com/arbuz/tictactoe/controller/GameController.java
@@ -6,8 +6,10 @@ import com.arbuz.tictactoe.model.GamePlay;
 import com.arbuz.tictactoe.controller.dto.ConnectRequest;
 import com.arbuz.tictactoe.exception.InvalidParamException;
 import com.arbuz.tictactoe.model.Game;
+import com.arbuz.tictactoe.model.GameStatus;
 import com.arbuz.tictactoe.model.Player;
 import com.arbuz.tictactoe.service.GameService;
+import com.arbuz.tictactoe.storage.GameRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -22,49 +24,50 @@ import org.springframework.web.bind.annotation.*;
 public class GameController {
     private final GameService gameService;
     private final SimpMessagingTemplate simpMessagingTemplate;
+    private final GameRepository gameRepository;
 
     @PostMapping("/start")
     public ResponseEntity<Game> start(@RequestBody Player player) {
         log.info("start game request: {}", player);
-
         return ResponseEntity.ok(gameService.createGame(player));
     }
 
     @PostMapping("/connect")
     public ResponseEntity<Game> connect(@RequestBody ConnectRequest request) throws InvalidParamException, InvalidGameException {
         log.info("connect request: {}", request);
-
         Game game = gameService.connectToGame(request.getPlayer(), request.getGameId());
         if (game != null) {
             notifyPlayers(game);
         }
-
         return ResponseEntity.ok(game);
     }
 
     @PostMapping("/connect/random")
     public ResponseEntity<Game> connectRandom(@RequestBody Player player) throws NotFoundException {
         log.info("connect random {}", player);
-
         Game game = gameService.connectToRandomGame(player);
         if (game != null) {
             notifyPlayers(game);
         }
-
         return ResponseEntity.ok(game);
     }
 
     @PostMapping("/gameplay")
     public ResponseEntity<Game> gamePlay(@RequestBody GamePlay request) throws NotFoundException, InvalidGameException {
         log.info("gameplay: {}", request);
-
         Game game = gameService.gamePlay(request);
         simpMessagingTemplate.convertAndSend("/topic/game-progress/" + game.getGameId(), game);
-
+        if (game.getStatus() == GameStatus.FINISHED) {
+            saveGame(game);
+        }
         return ResponseEntity.ok(game);
     }
 
     private void notifyPlayers(Game game) {
         simpMessagingTemplate.convertAndSend("/topic/game-progress/" + game.getGameId(), game);
+    }
+
+    private void saveGame(Game game) {
+        gameRepository.saveGame(game);
     }
 }
